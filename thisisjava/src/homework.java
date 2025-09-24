3213e5ef52dd5989ddb7e4a756134a65f6d397cc
calla1102 <calla20031@gmail.com>
2025-09-24 23:42:10 +0900

    add 4th assignment

---
+++ thisisjava/src/homework.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class homework {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		boolean run = true;
+		int money = 0;
+		
+		while(run) {
+			System.out.println("-----------------");
+			System.out.println("1.예금 | 2.출금 | 3.잔고 | 4.종료");
+			System.out.println("-----------------");
+			System.out.print("선택: ");
+			
+			String strNum = scanner.nextLine();
+
+			if(strNum.equals("1")) {
+				System.out.println("예금액> ");
+                int input = Integer.parseInt(scanner.nextLine().trim());
+				money += input;
+			}else if(strNum.equals("2")) {
+				System.out.println("출금액> ");
+                int input = Integer.parseInt(scanner.nextLine().trim());
+				money -= input;
+			}else if(strNum.equals("3")) {
+				System.out.println("잔고> "+money);
+			}else if(strNum.equals("4")) {
+				run = false ;
+			}
+		}
+		System.out.println("프로그램 종료");
+	}
+}
